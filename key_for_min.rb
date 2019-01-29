a2cdef0124ad9a931ebe188885a3fddf365ae673
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-29 21:42:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,7 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-    if name_hash == {}
+
+def key_for_min_value(nameHash)
+    if nameHash == {}
     return
   end
   value_array = name_hash.collect do |name, value|
